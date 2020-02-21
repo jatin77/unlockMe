5e2191387ecc33e2c41bd99b19d88f3ab3618190
jatin77 <clockstopfountain@gmail.com>
2020-02-21 15:50:26 +0530

    ...

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -7,27 +7,64 @@ import ChooseUs from "../../component/chooseUs/ChooseUs";
 import TransformBanner from "../../component/transformBanner/TransformBanner";
 import Footer from "../../component/footer/Footer";
 import FloatingGetApp from "../../component/floatinfGetApp/FloatingGetApp";
-const Home = () => {
-  return (
-    <>
-      <FloatingGetApp />
-      {/* Header slider */}
-      <HeaderSlides />
+class Home extends React.Component {
+  state = {
+    code: "",
+    errorMessage: ""
+  };
 
-      {/* carousel */}
-      <CoachCarousel />
-      {/* book coaching banner */}
-      <BookBanner />
-      {/* how it works */}
-      <WorksHow />
-      {/* transform banner */}
-      <TransformBanner />
-      {/* choose us section */}
-      <ChooseUs />
-      {/* footer above */}
-      <Footer />
-    </>
-  );
-};
+  handleSuccess = data => {
+    console.log(data);
+    this.setState({
+      code: data.code,
+      errorMessage: ""
+    });
+  };
+
+  handleFailure = error => {
+    console.log(error);
+
+    this.setState({
+      code: "",
+      errorMessage: error.errorMessage
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <FloatingGetApp />
+        {/* Header slider */}
+        <HeaderSlides />
+
+        {/* carousel */}
+        <CoachCarousel />
+        {/* book coaching banner */}
+        <BookBanner />
+        {/* how it works */}
+        <WorksHow />
+        {/* transform banner */}
+        <TransformBanner />
+        {/* choose us section */}
+        <ChooseUs />
+        {/* footer above */}
+        <Footer />
+        <LinkedIn
+          response_type="code"
+          clientId="817ew6hqc3jaeg"
+          scope="r_liteprofile r_emailaddress w_member_social"
+          onFailure={this.handleFailure}
+          onSuccess={this.handleSuccess}
+          redirectUri="https://dreamy-hawking-d497ff.netlify.com/"
+          renderElement={({ onClick, disabled }) => (
+            <button onClick={onClick} disabled={disabled}>
+              Custom linkedin element
+            </button>
+          )}
+        />
+      </>
+    );
+  }
+}
 
 export default Home;
